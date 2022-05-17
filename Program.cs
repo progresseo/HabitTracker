6d64f0db2a63bb9f46889583879a9ed01c48b318
progresseo <94923126+progresseo@users.noreply.github.com>
2022-05-17 16:16:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Habit_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
